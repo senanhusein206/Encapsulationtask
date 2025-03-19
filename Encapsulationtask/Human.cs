267db264025606517a7b522e3cd24cc8392b0983
senanhusein <sh8448266@gmail.com>
2025-03-19 10:02:50 +0400

    encapsulationtask

---
+++ Encapsulationtask/Human.cs
@@ -0,0 +1,98 @@
+﻿namespace Encapsulationtask;
+
+public class Human
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public byte age;
+    public byte Age
+    {
+        get { return age; }
+        set
+        {
+            if (value >= 0 && value <= 150)
+            {
+                age = value;
+            }
+            else
+            {
+                Console.WriteLine("bu yasda adam olar ala");
+            }
+        }
+        
+    }
+
+
+    public Human()
+    {
+
+    }
+
+    public Human(string name)
+    {
+        Name = name;
+    }
+    public Human(string name, string surname)
+    {
+        Name = name;
+        Surname = surname;
+    }
+
+
+    public void ShowFullData()
+    {
+        Console.WriteLine($"ad:{Name},  soyad:{Surname}, Age:{Age}");
+    }
+    public void GetFullName()
+    {
+        Console.WriteLine($"ad:{Name} soyad:{Surname}");
+    }
+
+}
+
+public class User:Human
+{
+    private string username { get; set; }
+private string password { get; set; }
+
+    public string UserName 
+{ 
+        get { return username; } 
+    set
+        {
+            if (value.Length >= 6 && value.Length <= 25)
+            {
+                username = value;
+
+            }
+            else
+            {
+                Console.WriteLine("daxil etdiyiniz username 6-25 simvol olmalidir");
+            }
+        } }
+    public string Password {
+        get { return password; }
+        set
+        {
+            if (value.Length >= 8 && value.Length <= 25)
+            {
+                password = value;
+
+            }
+            else
+            {
+                Console.WriteLine("Daxil etdiyiniz password 8-25 simvoldan ibaret olmalidir");
+            }
+
+
+        }
+    }
+
+    public User( string username, string password) 
+    {
+        UserName = username;
+        Password = password;
+    
+    }
+       
+    }
---
+++ Encapsulationtask/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Encapsulationtask;
+
+internal class Program
+{
+    static void Main(string[] args)
+
+      
+    {
+
+        Console.WriteLine("UserName daxil edin");
+        string username = Console.ReadLine();
+
+
+        Console.WriteLine("password daxil edin");
+        string password = Console.ReadLine();
+
+        User user = new User( username, password);
+
+        if (username.Length>=6 && username.Length<=25 && password.Length>=8 && password.Length<=25)
+        {
+            Console.WriteLine("istifadeci yaradildi");
+            Console.WriteLine($"Username:{username}");
+            Console.WriteLine($"Password:{password}");
+        }
+        else
+        {
+            Console.WriteLine("istifadeci yaradila bilmedi");
+           
+        }
+
+           
+    }
+}
